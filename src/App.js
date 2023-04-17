e905a19d2cd50afe04751a8c45f622cbb6bdbac8
InhooPark <park.linhoo@gmail.com>
2023-04-17 10:38:15 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Layout from "./Layout";
+import Volume from "./Volume";
 
 function App() {
+  const sounds = ["birds", "campfire", "lo-fi", "car", "footsteps", "rain", "thunder", "waves", "wind"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <main>
+          <section className="section">
+            <h1> Ambient </h1>
+            <article>
+              {sounds.map((sound, key) => {
+                return <Volume sound={sound} key={key} />;
+              })}
+            </article>
+          </section>
+        </main>
+      </Layout>
     </div>
   );
 }
---
+++ src/Volume.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+const Volume = ({ sound }) => {
+  const [volume, setVolume] = useState(0);
+  const [title, setTitle] = useState(sound);
+
+  function volumeFunc(e) {
+    setVolume(e.target.value);
+  }
+
+  useEffect(() => {
+    const target = document.getElementById(sound);
+    target.volume = volume / 100;
+  }, [volume]);
+  useEffect(() => {
+    setTitle(sound.charAt(0).toUpperCase() + sound.slice(1));
+  }, []);
+
+  return (
+    <div className="volume">
+      <p>{title}</p>
+      <audio id={`${sound}`} controls autoPlay loop src={`/sound/${sound}.mp3`} type="audio/mpeg" />
+      <input onChange={volumeFunc} type="range" max="100" value={volume} />
+    </div>
+  );
+};
+
+export default Volume;
